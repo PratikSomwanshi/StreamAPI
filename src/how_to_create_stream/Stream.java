0ec488a624304a623378f33944b68fc1a61571e1
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2025-01-12 01:45:55 +0530

    done with how to create Stream

---
+++ src/how_to_create_stream/Stream.java
@@ -0,0 +1,33 @@
+package how_to_create_stream;
+
+public class Stream {
+
+//        How to use stream
+//        Source, Intermediate operation and terminal operation
+
+//        1) using on collection
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+
+        Stream<Integer> stream = list.stream();
+
+//        2) using Arrays as source
+
+        int[] arr = {1,2,3,4,5,6,7};
+
+        IntStream stream = Arrays.stream(arr);
+
+        Stream<Integer> boxed = stream.boxed();
+
+//        3) using Stream.of()
+
+        Stream<String> strStream = Stream.of("a", "b", "c", "d", "e", "f", "g", "h");
+
+//        4) infinite stream
+
+        Stream<Double> randomInfiniteStream = Stream.generate(Math::random);
+
+        Stream<Integer> iterate = Stream.iterate(0, i -> i + 1);
+//        Above iterate accepts seed as 0 means starting point and UnaryOperator is a function which have  same data type for input  and return
+
+}
